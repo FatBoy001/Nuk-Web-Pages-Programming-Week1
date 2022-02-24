c12ff0805d97f3b96466f467cdc3a4a8ec000216
FatBoy001 <51392131+FatBoy001@users.noreply.github.com>
2022-02-24 11:17:39 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+
+    </head>
+    
+    <body>
+        <?php
+            $i = 2;
+            $j = 4;
+            echo "Hello World</br>";            
+            echo $i * $j;
+        ?>
+    </body>
+</html>
